880ac868ae3106794395fa5e7fb7c56b753406bf
Dragon <damianoss.c@gmail.com>
2014-10-21 22:13:59 +0200

    Ulepszenie css

--- src/main/java/com/example/servletjspdemo/tramwaj/StronaTytulowa.java
+++ src/main/java/com/example/servletjspdemo/tramwaj/StronaTytulowa.java
@@ -25,22 +25,28 @@ public class StronaTytulowa extends HttpServlet {
 				+"<html>"
 				+"<head>"
 					+"<script src='Sprawdzenie.js' type='text/javascript'></script>"
+					+"<link href=\"http://netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css\" rel=\"stylesheet\">"
 					+"<link href='StylTram.css' rel='stylesheet' type='text/css'>"
 				+"<head/>"
 					+ "<body>"
-						+ "<h2>Program Tramwaj</h2>"
+						+ "<h2><center>Program Tramwaj</center></h2>"
 						+ "<div class='container' id='panel_gorny'>"
-						+ "<a href='Tramwaj1' role='button' id='button'>"
+						+ "<a href='Tramwaj1' class=\"btn btn-primary btn-lg\" role='button' id='button'>"
 						+ "Strona główna"
 						+ "</a>"
+						
+						+ "<a href='WszystkieTramwaje1' class=\"btn btn-primary btn-lg\" role='button' id='button'>"
+						+ "Lista tramwajów"
+						+ "</a>"
+						
 						+ "</div>"
 						
 						
 						+ "<div class='container' id='tresc_strony'>"
 						+ "<form action='OdnosnikTramwaj1' name='formularz' method='get'>" +
 						
-						"Podaj Firme: <input type='textbox' name='firma' value='' required><br />" +
-						"Podaj Model: <input type='textbox' name='model' value='' required><br />" +
+						"Podaj Firme: <input type='textbox' id='button' name='firma' value='' required><br />" +
+						"Podaj Model: <input type='textbox' id='button' name='model' value='' required><br />" +
 						"Podaj Kolor: <br />" +
 						"<select name='select' multiple='multiple' defaultvalue>"+
 
@@ -54,16 +60,16 @@ public class StronaTytulowa extends HttpServlet {
 						"</select>"+
 						"<br /><br />"+
 						
-						"Podaj Liczbe miejsc: <input type='textbox' name='liczbaMiejsc' value='' required><br />" +
+						"Podaj Liczbe miejsc: <input type='textbox' id='button' name='liczbaMiejsc' value='' required><br />" +
 						
 						
 						"<br />"+
 						"Rodzaj: <br />Towarowy <input type='radio' name='rodzaj' value='towarowy' checked />"+
 						"<br />Pasazerski <input type='radio' name='rodzaj' value='pasazerski' /><br /><br />"+
 						
-						"Podaj adres zdjecia: <input type='textbox' name='zdjecie' value='' required><br /><br />" +
+						"Podaj adres zdjecia: <input type='textbox' id='button' name='zdjecie' value='' required><br /><br />" +
 						
-						"<input type='submit' onClick='return(sprawdz_formularz());' value=' OK ' />" +
+						"<input type='submit' id='button' class='btn btn-default' onClick='return(sprawdz_formularz());' value=' OK ' />" +
 						"</form>" 
 						+ "</div>"
 						
