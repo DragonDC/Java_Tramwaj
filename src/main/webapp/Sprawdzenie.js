6f3215452ee68865fe8b0d869e30b5d394b8652d
Dragon <damianoss.c@gmail.com>
2014-10-21 21:40:29 +0200

    update

---
+++ src/main/webapp/Sprawdzenie.js
@@ -0,0 +1,19 @@
+/**
+ * +"<script type='text/javascript' src='Sprawdzenie.js'></script>"
+ */
+	
+		function sprawdz_formularz()
+		{
+			var firma = document.forms["formularz"]["liczbaMiejsc"].value;
+			var wyrazenie = new RegExp("^[0-9]+$");
+			var bledy;
+			//return wyrazenie.test(firma);
+			if(wyrazenie.test(firma) == false){
+				bledy='Nieprawidłowy format dla liczby miejsc';
+				alert(bledy);
+				return false;
+			}
+			else
+				return true;
+     			
+		}
