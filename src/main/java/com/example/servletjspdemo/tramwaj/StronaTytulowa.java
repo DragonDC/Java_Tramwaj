2aa902c1e1aa496c95375ba533b36a137a128894
Dragon <damianoss.c@gmail.com>
2014-10-21 20:43:42 +0200

    wersja z javascript

--- src/main/java/com/example/servletjspdemo/tramwaj/StronaTytulowa.java
+++ src/main/java/com/example/servletjspdemo/tramwaj/StronaTytulowa.java
@@ -24,14 +24,14 @@ public class StronaTytulowa extends HttpServlet {
 		out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">"
 				+"<html>"
 				+"<head>"
-					+"<script type='text/html' src='Sprawdzenie.js'>"
+					+"<script src='Sprawdzenie.js' type='text/javascript'></script>"
 				+"<head/>"
 					+ "<body>"
 						+ "<h2>Program Tramwaj</h2>"
 						+"<form action='OdnosnikTramwaj1' name='formularz' method='get'>" +
 						
-						"Podaj Firme: <input type='textbox' name='firma' value=''><br />" +
-						"Podaj Model: <input type='textbox' name='model' value=''><br />" +
+						"Podaj Firme: <input type='textbox' name='firma' value='' required><br />" +
+						"Podaj Model: <input type='textbox' name='model' value='' required><br />" +
 						"Podaj Kolor: <br />" +
 						"<select name='select' multiple='multiple' defaultvalue>"+
 
@@ -45,16 +45,16 @@ public class StronaTytulowa extends HttpServlet {
 						"</select>"+
 						"<br /><br />"+
 						
-						"Podaj Liczbe miejsc: <input type='textbox' name='liczbaMiejsc' value=''><br />" +
+						"Podaj Liczbe miejsc: <input type='textbox' name='liczbaMiejsc' value='' required><br />" +
 						
 						
 						"<br />"+
 						"Rodzaj: <br />Towarowy <input type='radio' name='rodzaj' value='towarowy' checked />"+
 						"<br />Pasazerski <input type='radio' name='rodzaj' value='pasazerski' /><br /><br />"+
 						
-						"Podaj adres zdjecia: <input type='textbox' name='zdjecie' value=''><br /><br />" +
+						"Podaj adres zdjecia: <input type='textbox' name='zdjecie' value='' required><br /><br />" +
 						
-						"<input type='submit' onclick='sprawdz_formularz' value=' OK ' />" +
+						"<input type='submit' onClick='return(sprawdz_formularz());' value=' OK ' />" +
 						"</form>" 
 						
 						
